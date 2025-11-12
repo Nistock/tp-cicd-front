5337f1a88f0711eb7d65f1a90c6f63dba78bce8b
Nistock <130464319+Nistock@users.noreply.github.com>
2025-11-12 17:03:13 +0100

    modif

--- eslint.config.ts
+++ eslint.config.ts
@@ -12,7 +12,6 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
-    rules: {"vue/multi-word-component-names": "off"},
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'src/env.d.ts']),
